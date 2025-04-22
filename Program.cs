5ccbf0874b416c68a660229c349a42c88642f14e
Maluska10 <salamona@csp.edu>
2025-04-21 21:20:47 -0500

    Added constants

--- Program.cs
+++ Program.cs
@@ -14,126 +14,129 @@ namespace _2D_Arrays
             const string FACES = "\u263A";
             const string SYMBOLS_MODE = "1";
             const string NUMBERS_MODE = "2";
+            const string SYMBOL_CHOICE_HEARTS = "hearts";
+            const string SYMBOL_CHOICE_FACES = "faces";
+            const string INVALID_MESSAGE = @"Invalid. Please enter ""hearts"" or ""faces"".";
             int rows = 0;
             int columns = 0;
 
             Console.WriteLine("Welcome! \nWhat modes would you like to run?\nPress \"1\" for symbols or \"2\" for number.");
             string modeChoice = Console.ReadLine();
 
-            while (modeChoice != "1" && modeChoice != "2")
+            while (modeChoice != SYMBOLS_MODE && modeChoice != NUMBERS_MODE)
             {
                 Console.WriteLine("Incorrect input. Please try again.");
                 modeChoice = Console.ReadLine();
             }
 
-                Console.WriteLine("How many rows do you want to enter?");
-                rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many rows do you want to enter?");
+            rows = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("How many columns do you want to enter?");
-                columns = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many columns do you want to enter?");
+            columns = Convert.ToInt32(Console.ReadLine());
 
-                grid = new int[rows, columns];
-
-
-                width = 2 * columns + 1;
-                string border = new string('*', width);
-               
+            grid = new int[rows, columns];
 
 
-                if (modeChoice == "1")
+            width = 2 * columns + 1;
+            string border = new string('*', width);
+
+
+
+            if (modeChoice == SYMBOLS_MODE)
+            {
+
+                Console.WriteLine("What symbol you'd like to use: hearts or faces?");
+                Console.WriteLine("1. Hearts");
+                Console.WriteLine("2. Faces");
+                string choice = Console.ReadLine();
+                string symbol = "";
+
+                while (symbol == "")
                 {
-
-                    Console.WriteLine("What symbol you'd like to use: hearts or faces?");
-                    Console.WriteLine("1. Hearts");
-                    Console.WriteLine("2. Faces");
-                    string choice = Console.ReadLine();
-                    string symbol = "";
-
-                    while (symbol == "")
+                    if (choice.ToLower() == SYMBOL_CHOICE_HEARTS)
                     {
-                        if (choice.ToLower() == "hearts")
-                        {
-                            symbol = HEARTS;
-                        }
-
-                        if (choice.ToLower() == "faces")
-                        {
-                            symbol = FACES;
-                        }
-                        else
-                        {
-                            Console.WriteLine(@"Invalid. Please enter ""hearts"" or ""faces"".");
-                            choice = Console.ReadLine();
-
-                        }
-
+                        symbol = HEARTS;
                     }
 
-                    Console.WriteLine(border, Color.Gold);
-
-                    for (int i = 0; i < rows; i++)
-
+                    if (choice.ToLower() == SYMBOL_CHOICE_FACES)
                     {
-
-                        for (int j = 0; j < columns; j++)
-                        {
-                            if (j % 2 != 0)
-                            {
-
-                                Console.Write("*" + symbol, Color.Red);
-
-
-                            }
-                            else
-                            {
-
-                                Console.Write("*" + symbol, Color.Gold);
-
-                            }
-
-                        }
-                        Console.WriteLine("*", Color.Gold);
+                        symbol = FACES;
+                    }
+                    else
+                    {
+                        Console.WriteLine(INVALID_MESSAGE);
+                        choice = Console.ReadLine();
 
                     }
-                    Console.WriteLine(border, Color.Gold);
-
 
                 }
 
+                Console.WriteLine(border, Color.Gold);
 
+                for (int i = 0; i < rows; i++)
 
-                if (modeChoice == "2")
                 {
+
+                    for (int j = 0; j < columns; j++)
+                    {
+                        if (j % 2 != 0)
+                        {
+
+                            Console.Write("*" + symbol, Color.Red);
+
+
+                        }
+                        else
+                        {
+
+                            Console.Write("*" + symbol, Color.Gold);
+
+                        }
+
+                    }
+                    Console.WriteLine("*", Color.Gold);
+
+                }
+                Console.WriteLine(border, Color.Gold);
+
+
+            }
+
+
+
+            if (modeChoice == NUMBERS_MODE)
+            {
 
                 for (int j = 0; j < columns; j++)
                 {
                     Console.Write("X----", Color.Green);
-                    
+
 
                 }
                 Console.Write("X", Color.Green);
                 Console.Write("\n");
 
                 for (int i = 0; i < rows; i++)
-                    {
+                {
                     if (i > 0)
                     {
 
-                        
-                            for (int j = 0; j < columns; j++)
-                            {
-                                Console.Write(" ----", Color.Red);
 
-                            }
-                            Console.WriteLine();
+                        for (int j = 0; j < columns; j++)
+                        {
+                            Console.Write(" ----", Color.Red);
+
                         }
+                        Console.WriteLine();
+                    }
                     for (int j = 0; j < columns; j++)
                     {
-                        
 
-                            Console.Write("| " + " "+ j + " ", Color.Red);
-                        
-                   
+
+                        Console.Write("| " + " " + j + " ", Color.Red);
+
+
 
                     }
 
@@ -144,7 +147,7 @@ namespace _2D_Arrays
 
                 for (int j = 0; j < columns; j++)
                 {
-                    
+
                     Console.Write("X----", Color.Green);
 
                 }
@@ -160,12 +163,12 @@ namespace _2D_Arrays
 
         }
 
-                
-                
-            
-                
-            
 
 
-        }
+
+
+
+
+
     }
+}
